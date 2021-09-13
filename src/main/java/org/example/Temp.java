3859b11a775c437606ba816dd20002cc3dc3d824
Celina Alzenor <you@example.com>
2021-09-12 21:20:55 -0400

    Initial commit

---
+++ src/main/java/org/example/Temp.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Temp Class file
+ *  Copyright 2021 Celina Alzenor
+ */
+
+package org.example;
+import java.util.Scanner;
+
+public class Temp
+{
+    private final String choice;
+
+    //asks if user wants what temperature unit to convert to
+    public Temp()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter C to convert from Fahrenheit to Celsius\nEnter F to convert from Celsius to Fahrenheit");
+        System.out.print("Your choice: ");
+        this.choice = input.next();
+    }
+
+    public void Converter()
+    {
+        //converts fahrenheit to celsius and prints the result
+        if(this.choice.matches("C") || this.choice.matches("c")) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Please enter the temperature in Fahrenheit: ");
+            int fah = input.nextInt();
+
+            double cel = (fah - 32) * (5.0/9.0);
+            System.out.print("The temperature in Celsius is " + (int) cel);
+        }
+
+        //converts celsius to Fahrenheit and prints the result
+        if(this.choice.matches("F") || this.choice.matches("f"))
+        {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Please enter the temperature in Celsius: ");
+            int cel = input.nextInt();
+
+            double fah = (cel * (5.0/9.0)) - 32 ;
+            System.out.print("The temperature in Fahrenheit is "+(int) fah);
+        }
+    }
+}
